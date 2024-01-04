76c2ed7d3632a7e82ab28939ab3a8ad9d9de74fa
Parsa <psaeedi1381@gmail.com>
2024-01-03 18:45:47 -0800

    FE: Compress image before POST

--- client/src/useGeneratePaletteAPI.js
+++ client/src/useGeneratePaletteAPI.js
@@ -1,11 +1,33 @@
 import axios from 'axios';
+import Resizer from "react-image-file-resizer";
+
+const MAXWIDTH = 500;
+const MAXHEIGHT = 500;
 
 export default function useGeneratePaletteAPI() {
+
+    async function compressImage(image) {
+        return new Promise((resolve) => {
+            Resizer.imageFileResizer(
+                image,
+                MAXWIDTH,
+                MAXHEIGHT,
+                "JPEG",
+                70,
+                0,
+                (uri) => {
+                    resolve(uri);
+                },
+                "file"
+            );
+        });
+    }
 
     async function fetchPalette(selectedFile, numOfColors) {
 
         const bodyFormData = new FormData();
-        bodyFormData.append('file', selectedFile);
+        const compressedImage = await compressImage(selectedFile);
+        bodyFormData.append('file', compressedImage);
         bodyFormData.append('numClusters', numOfColors);
 
         return new Promise((resolve, reject) => {
