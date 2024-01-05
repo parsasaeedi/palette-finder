2386cf8e980c93ea8c0fc8790f0610b6445a5a99
Parsa <psaeedi1381@gmail.com>
2024-01-05 15:16:02 -0800

    FE: Add download button

--- client/src/PalettePage.js
+++ client/src/PalettePage.js
@@ -1,6 +1,11 @@
 import { Button, ConfigProvider, message, Tooltip } from 'antd';
+import { exportComponentAsPNG } from "react-component-export-image";
+import React, { useRef } from "react";
 
 export default function PalettePage(props) {
+
+    const componentRef = useRef();
+
     const numberSign = '#'
     const buttonTheme = {
         components: {
@@ -12,6 +17,13 @@ export default function PalettePage(props) {
     }
     const zeroPad = (num, places) => String(num).padStart(places, '0');
     const luminance = (color) => {return color.reduce((a, b) => a + b, 0)/3}
+    const downloadOptions = {
+        fileName: 'Palette',
+        html2CanvasOptions: {
+            backgroundColor: null,
+            scale: 4,
+        }
+    }
 
     function copyTextToClipboard(text) {
         if ('clipboard' in navigator) {
@@ -22,33 +34,38 @@ export default function PalettePage(props) {
         }
     };
 
+    const Palette = React.forwardRef((props, ref) => (
+        <div ref={ref} className='image-palette-container'>
+            {props.selectedFile != null && 
+                <img className='palette-image' src={URL.createObjectURL(props.selectedFile)} />
+            }
+            <div className='palette'>
+                {props.palette.map((color) => (
+                    <div 
+                        className= {luminance(color) >= 100 ? 'palette-color palette-color-bright' : 'palette-color palette-color-dark'}
+                        style={{backgroundColor: numberSign.concat(color.map(channel => zeroPad(channel.toString(16), 2)).join(''))}}
+                        onClick={() => copyTextToClipboard(color.map(channel => zeroPad(channel.toString(16), 2)).join('').toUpperCase())}
+                    >
+                        <Tooltip title="Click to copy" placement="bottom" className='palette-color-text-tooltip'>
+                            <div className='palette-color-text'>
+                                {color.map(channel => zeroPad(channel.toString(16), 2)).join('').toUpperCase()}
+                            </div>
+                        </Tooltip>
+                    </div>
+                ))}
+            </div>
+        </div>
+    ));
+
     return (
         <div className='palette-page'>
             <h1 className='logo' onClick={props.replaceImage}>PaletteFinder</h1>
-            <div className='image-palette-container'>
-                {props.selectedFile != null && 
-                    <img className='palette-image' src={URL.createObjectURL(props.selectedFile)} />
-                }
-                <div className='palette'>
-                    {props.palette.map((color) => (
-                        <div 
-                            className= {luminance(color) >= 100 ? 'palette-color palette-color-bright' : 'palette-color palette-color-dark'}
-                            style={{backgroundColor: numberSign.concat(color.map(channel => zeroPad(channel.toString(16), 2)).join(''))}}
-                            onClick={() => copyTextToClipboard(color.map(channel => zeroPad(channel.toString(16), 2)).join('').toUpperCase())}
-                        >
-                            <Tooltip title="Click to copy" placement="bottom" className='palette-color-text-tooltip'>
-                                <div className='palette-color-text'>
-                                    {color.map(channel => zeroPad(channel.toString(16), 2)).join('').toUpperCase()}
-                                </div>
-                            </Tooltip>
-                        </div>
-                    ))}
-                </div>
-            </div>
+            <Palette ref={componentRef} selectedFile={props.selectedFile} palette={props.palette}/>
             <ConfigProvider theme={buttonTheme} >
                 <div className='palette-page-buttons'>
-                    <Button className='settings-button' type="primary" shape="round" size='large' onClick={() => props.setIsSettingsModalOpen(true)}>Palette Settings</Button>
-                    <Button className='replace-image-button' type="primary" shape="round" size='large' onClick={props.replaceImage}>Replace Image</Button>
+                    <Button className='palette-page-button' type="primary" shape="round" size='large' onClick={() => props.setIsSettingsModalOpen(true)}>Palette Settings</Button>
+                    <Button className='palette-page-button' type="primary" shape="round" size='large' onClick={props.replaceImage}>Replace Image</Button>
+                    <Button className='palette-page-button download-button' type="primary" shape="round" size='large' onClick={() => exportComponentAsPNG(componentRef, downloadOptions)}>Download Palette</Button>
                 </div>
             </ConfigProvider>
         </div>
