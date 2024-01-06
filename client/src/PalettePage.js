2c67e8fc833138fcd1dc7e9a648f2513fb470fa0
Parsa <psaeedi1381@gmail.com>
2024-01-05 18:59:19 -0800

    FE: Fix settings button flickering

--- client/src/PalettePage.js
+++ client/src/PalettePage.js
@@ -2,60 +2,59 @@ import { Button, ConfigProvider, message, Tooltip } from 'antd';
 import { exportComponentAsPNG } from "react-component-export-image";
 import React, { useRef } from "react";
 
+const numberSign = '#'
+const buttonTheme = {
+    components: {
+        Button: {
+            colorPrimaryHover: '#8E8E8E',
+            colorTextLightSolid: '#000000'
+        },
+        },
+}
+const downloadOptions = {
+    fileName: 'Palette',
+    html2CanvasOptions: {
+        backgroundColor: null,
+        scale: 4,
+    }
+}
+const zeroPad = (num, places) => String(num).padStart(places, '0');
+const luminance = (color) => {return color.reduce((a, b) => a + b, 0)/3}
+function copyTextToClipboard(text) {
+    if ('clipboard' in navigator) {
+        navigator.clipboard.writeText(text);
+        message.success("Color #" + text + " copied to clipboard")
+    } else {
+        document.execCommand('copy', true, text);
+    }
+};
+
+const Palette = React.forwardRef((props, ref) => (
+    <div ref={ref} className='image-palette-container'>
+        {props.selectedFile != null && 
+            <img className='palette-image' src={URL.createObjectURL(props.selectedFile)} />
+        }
+        <div className='palette'>
+            {props.palette.map((color) => (
+                <div 
+                    className= {luminance(color) >= 100 ? 'palette-color palette-color-bright' : 'palette-color palette-color-dark'}
+                    style={{backgroundColor: numberSign.concat(color.map(channel => zeroPad(channel.toString(16), 2)).join(''))}}
+                    onClick={() => copyTextToClipboard(color.map(channel => zeroPad(channel.toString(16), 2)).join('').toUpperCase())}
+                >
+                    <Tooltip title="Click to copy" placement="bottom" className='palette-color-text-tooltip'>
+                        <div className='palette-color-text'>
+                            {color.map(channel => zeroPad(channel.toString(16), 2)).join('').toUpperCase()}
+                        </div>
+                    </Tooltip>
+                </div>
+            ))}
+        </div>
+    </div>
+));
+
 export default function PalettePage(props) {
 
     const componentRef = useRef();
-
-    const numberSign = '#'
-    const buttonTheme = {
-        components: {
-            Button: {
-                colorPrimaryHover: '#8E8E8E',
-                colorTextLightSolid: '#000000'
-            },
-          },
-    }
-    const zeroPad = (num, places) => String(num).padStart(places, '0');
-    const luminance = (color) => {return color.reduce((a, b) => a + b, 0)/3}
-    const downloadOptions = {
-        fileName: 'Palette',
-        html2CanvasOptions: {
-            backgroundColor: null,
-            scale: 4,
-        }
-    }
-
-    function copyTextToClipboard(text) {
-        if ('clipboard' in navigator) {
-            navigator.clipboard.writeText(text);
-            message.success("Color #" + text + " copied to clipboard")
-        } else {
-            document.execCommand('copy', true, text);
-        }
-    };
-
-    const Palette = React.forwardRef((props, ref) => (
-        <div ref={ref} className='image-palette-container'>
-            {props.selectedFile != null && 
-                <img className='palette-image' src={URL.createObjectURL(props.selectedFile)} />
-            }
-            <div className='palette'>
-                {props.palette.map((color) => (
-                    <div 
-                        className= {luminance(color) >= 100 ? 'palette-color palette-color-bright' : 'palette-color palette-color-dark'}
-                        style={{backgroundColor: numberSign.concat(color.map(channel => zeroPad(channel.toString(16), 2)).join(''))}}
-                        onClick={() => copyTextToClipboard(color.map(channel => zeroPad(channel.toString(16), 2)).join('').toUpperCase())}
-                    >
-                        <Tooltip title="Click to copy" placement="bottom" className='palette-color-text-tooltip'>
-                            <div className='palette-color-text'>
-                                {color.map(channel => zeroPad(channel.toString(16), 2)).join('').toUpperCase()}
-                            </div>
-                        </Tooltip>
-                    </div>
-                ))}
-            </div>
-        </div>
-    ));
 
     return (
         <div className='palette-page'>
