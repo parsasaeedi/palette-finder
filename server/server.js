1bf0f7e1cfb600d3cd40e884bf2a48a6241b70de
Parsa <psaeedi1381@gmail.com>
2024-01-05 20:45:47 -0800

    BE: Set port to env variable

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const multer = require('multer');
 
 const app = express()
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
